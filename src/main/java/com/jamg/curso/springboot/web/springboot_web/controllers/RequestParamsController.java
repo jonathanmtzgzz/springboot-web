03c5b390cbd9fd8fa33d038ac9f25484575f63c5
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2024-10-01 18:43:11 -0600

    feat: Add RequestParamsController with foo endpoint and implement ParamDto to encapsulate the message data

---
+++ src/main/java/com/jamg/curso/springboot/web/springboot_web/controllers/RequestParamsController.java
@@ -0,0 +1,22 @@
+package com.jamg.curso.springboot.web.springboot_web.controllers;
+
+
+import com.jamg.curso.springboot.web.springboot_web.models.dto.ParamDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+    @GetMapping("/foo")
+    public ParamDto foo(@RequestParam(
+            required = false,
+            defaultValue = "Hello what's up") String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+}
---
+++ src/main/java/com/jamg/curso/springboot/web/springboot_web/models/dto/ParamDto.java
@@ -0,0 +1,12 @@
+package com.jamg.curso.springboot.web.springboot_web.models.dto;
+
+public class ParamDto {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

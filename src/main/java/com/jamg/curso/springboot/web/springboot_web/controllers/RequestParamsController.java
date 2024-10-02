d58463b3502054bdc4664482830b0c53ea5a9dad
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2024-10-01 20:35:33 -0600

    feat: Add bar and request endpoints to RequestParamsController

--- src/main/java/com/jamg/curso/springboot/web/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/jamg/curso/springboot/web/springboot_web/controllers/RequestParamsController.java
@@ -2,6 +2,8 @@ package com.jamg.curso.springboot.web.springboot_web.controllers;
 
 
 import com.jamg.curso.springboot.web.springboot_web.models.dto.ParamDto;
+import com.jamg.curso.springboot.web.springboot_web.models.dto.ParamMixDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,4 +21,26 @@ public class RequestParamsController {
         param.setMessage(message);
         return param;
     }
+
+    @GetMapping("/bar")
+    public ParamMixDto bar(@RequestParam String text, @RequestParam Integer code) {
+        ParamMixDto params = new ParamMixDto();
+        params.setText(text);
+        params.setCode(code);
+        return params;
+    }
+
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request) {
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {
+        }
+        ParamMixDto params = new ParamMixDto();
+        params.setText(request.getParameter("text"));
+        params.setCode(code);
+        return params;
+    }
+
 }
---
+++ src/main/java/com/jamg/curso/springboot/web/springboot_web/models/dto/ParamMixDto.java
@@ -0,0 +1,21 @@
+package com.jamg.curso.springboot.web.springboot_web.models.dto;
+
+public class ParamMixDto {
+
+    private String text;
+    private Integer code;
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
